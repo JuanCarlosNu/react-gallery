73f39612a45549e466bf03ec26618ddf4f2f3731
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-10-05 17:54:28 -0300

    123. 06-get the data from the api

--- src/component/PhotoList/PhotoList.js
+++ src/component/PhotoList/PhotoList.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-
-
+import React, { Component } from 'react'
+import axios from 'axios';
+const API_KEY= "NtCC0KXu88qlLdrN_eers1JczYKsAdeYR1LipS7FH80"
 
 class PhotoList extends Component {
 
@@ -11,8 +11,14 @@ class PhotoList extends Component {
         }
     }
   
-    componentDidMount(){
-        
+    async componentDidMount(){
+         const data = await axios.get('https://api.unsplash.com/photos', {
+            headers: {
+                Authorization: "Client-ID " + API_KEY
+            }
+          
+        });
+        console.log(data);
     }
     render() { 
         return (
