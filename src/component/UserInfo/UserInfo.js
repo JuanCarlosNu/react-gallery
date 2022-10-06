18b2a0b871a6722913ba87ac85a05750cf7c98dd
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-10-06 16:05:29 -0300

    127. 10-define our routes

---
+++ src/component/UserInfo/UserInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class UserInfo extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>User</div>
+
+    }
+}
+ 
+export default UserInfo;
+
