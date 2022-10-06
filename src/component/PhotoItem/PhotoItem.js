4a04779a37f6a39ad6ccf8aa7ef9e88fa9eabdb4
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-10-06 15:02:22 -0300

    127. 10 install react-router-dom and imported to the index.js file

--- src/component/PhotoItem/PhotoItem.js
+++ src/component/PhotoItem/PhotoItem.js
@@ -4,17 +4,19 @@ const PhotoItem = ({ photo }) => {
   return (
     <div>
      <div className='photo__img--container'>
-         <img/>
+         <img src={photo.urls.thumb} className='photo__img' alt={photo.alt_description}/>
       </div>
      <div className='photo__content'>
-         <p className='photo__time'></p>
-         <p className='photo__time'></p>
+         <p className='photo__time' >{photo.created_at}</p>
+         <p className='photo__time'>{photo.updated_at}</p>
          <p className='photo__likes'></p>
          <p className='photo__user'></p>
       </div>
       <div className='photo__buttons'>
-         <a className='photo__btn'></a>
-         <a className='photo__btn'></a>
+         <a className='photo__btn' href={photo.user.links.html}>{photo.user.name}</a>
+         <a className='photo__btn' href={photo.links.html}>
+            view image
+         </a>
       </div>
 
     </div>
