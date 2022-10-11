ea33c9375c7d3aec4483964daf1cda06d49cfb86
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-10-11 16:35:19 -0300

    129. 12-get the username from the url

--- src/component/UserInfo/UserInfo.js
+++ src/component/UserInfo/UserInfo.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 
 
 class UserInfo extends Component {
-    state = {  } 
+
+    constructor(){
+        super()
+        this.state = {
+            user: {}
+          }
+        }
+     
+        componentDidMount(){
+            const username = this.props.match.params.username
+             console.log(this.props)
+        }
+
     render() { 
         return (
             <div>User</div>
