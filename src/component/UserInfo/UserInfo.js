d5bcdf69f8515416bed64f53b87a179eda8ed5f0
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-10-13 14:55:21 -0300

    135. show user info part 3: add profile link conditional. And get some isue with the "loading" signal.

--- src/component/UserInfo/UserInfo.js
+++ src/component/UserInfo/UserInfo.js
@@ -25,17 +25,19 @@ class UserInfo extends Component {
     render() {  
         const { user } = this.state;
 
-        if (!user) return "Loading";
+        if (!user) return "Loading"; //when loading we are getting view profile instead of loading, that is becase we get some elements from the array so the browser display the rendering and we get the local text before we get the image(couse it is more heavy)
  
 
         return (
             <div className='user__info'>
                 <div className='user__content'>
                     <h1 className='user__title'>{user.name}</h1>
-                    <p className='user__bio' >{user.value}</p>
+                    <p className='user__bio' >{user.bio}</p>
                     <div className='user__img--container'>
-                        <img src={user.profile_image? user.profile_image.large : ""} alt={user.name}  className='user__img' />
-                            <a className='user__profile'></a>
+                        <img src={user.profile_image? user.profile_image.large : ""}
+                         alt={user.name}  
+                         className='user__img' />
+                        <a href={user.links?user.links.html: ""} className='user__profile'>view Profile</a>
                     </div>
                 </div>
                 <div className='user__photos--container'>
