acc766eced4cc0e9e109e2d67a1b36174c4f3f28
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-10-14 14:46:41 -0300

    139. 22-add our select input to the form

--- src/component/PhotoForm/PhotoForm.js
+++ src/component/PhotoForm/PhotoForm.js
@@ -3,7 +3,17 @@ import React from 'react';
 
 const PhotoForm = () => {
     return ( 
-    <div>PhotoForm</div>
+    <form>
+        <select name='sortTerm' className='Select'>
+            <option value="created_at">Created at</option>
+            <option value="updated_at">Updated at</option>
+            <option value="likes">Select Item</option>
+        </select>
+        <select>
+            <option value="asc">Asc</option>
+            <option value="desc">Desc</option>
+        </select>
+    </form>
      );
 }
  
